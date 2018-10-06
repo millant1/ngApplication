c5bf6eb5c03ca95bb6f144270ef845a134c23da3
Tom M/ <tom10491@yahoo.com>
2018-10-06 19:52:34 -0400

    updates

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,14 +12,14 @@ export class RecipeService {
     new Recipe(
       'Tasty Schnitzel',
       'A super-tasty Schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      'https://www.recipetineats.com/wp-content/uploads/2017/08/Schnitzel-3-landscape.jpg',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe('Big Fat Burger',
       'What else you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      'https://digitalmarketing.blob.core.windows.net/5926/images/items/image208546.jpg',
       [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)
